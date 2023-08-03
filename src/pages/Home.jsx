c10c074f2e99b566998bd95ea4db466523f033cb
Aditya <adityashah192105@gmail.com>
2023-08-03 23:40:10 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar';
 import BookCard from '../components/BookCard'
 import Search from '../components/search'
 import imgA from '../components/images/ADSA.png'
-import Quickfilter from '../components/QuickFilter'
+
 
 
 const notes = [
@@ -46,7 +46,7 @@ const Home = () => {
       
       <div className=' bg-black dark:bg-gray-900  w-full z-20 top-0 left-0 border-b border border-solid border-gray-800 dark:border-gray-600 h-screen'>
         <div className='text-white container mx-auto mt-3'>
-        <Quickfilter/>
+        
         <Search/><br/>
         
       
