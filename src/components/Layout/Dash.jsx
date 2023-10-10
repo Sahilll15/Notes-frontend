1f6b77ac069ac702132a6d6c4a13387c0167d803
Aditya <192105adityashah@gmail.com>
2023-10-10 22:00:08 +0530

    added sidebar layout to most pages

--- src/components/Layout/Dash.jsx
+++ src/components/Layout/Dash.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const MainLayout = ({ children }) => {
   return (
-    <div className="flex justify-center ">
-      <div className="w-full  md:w-2/3 lg:w-2/4">
+    <div className="flex justify-center -mt-16 bg-gradient-to-r from-gray-800 to-gray-600 -mb-[5rem] lg:h-screen sm:h-full md:h-full ">
+      <div className="w-full  md:w-2/3 mt-14 lg:w-2/4  ">
         {children}
       </div>
     </div>
