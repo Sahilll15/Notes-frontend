33c96ce129f731cf64d9e27faa62b1262c300466
Aditya <192105adityashah@gmail.com>
2023-10-14 18:45:15 +0530

    Added modal

--- src/pages/Newdashboard.jsx
+++ src/pages/Newdashboard.jsx
@@ -1,11 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import DashLay from "../components/Layout/Dash";
 import { getUserInfo, getUsersLeaderBoard } from "../redux/user/userActions";
 import { useDispatch, useSelector } from "react-redux";
 import Lottery from "../components/Lottery";
+import SendMoneyCard from "../components/SendMoney";
+
 
 const Newdashboard = () => {
   const dispatch = useDispatch();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  }
+
+  const closeModal = () => {
+    setModalOpen(false);
+  }
   const user = useSelector((state) => state.user.user);
   const userDetails = useSelector((state) => state.userDetails.userDetails);
   const userDetailsLoading = useSelector(
@@ -27,6 +38,7 @@ const Newdashboard = () => {
     <div>
       <DashLay>
         <div>{/* send coins , Bookmarked , your notes */}</div>
+        
         <div className="flex-col lg:flex lg:flex-row gap-12 mb-4">
           <div class="flex items-center  justify-center lg:w-4/12  w-full h-28 rounded-xl bg-gray-50  mt-2 pl-3">
             <i class="fa-solid fa-ranking-star text-yellow-600 fa-xl"></i>
@@ -54,41 +66,47 @@ const Newdashboard = () => {
             </h3>
           </div>
         </div>
-        <div class="flex justify-center py-3 ">
-          <div class="flex flex-col items-center sm:flex-row mb-9">
-            <div class="relative flex flex-col rounded-xl bg-gradient-to-r from-yellow-100 to-yellow-500 bg-clip-border text-yellow-900 shadow-md">
-              <div class="p-6">
-                <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal">
-                  Send Coins &nbsp;
-                </h5>
-              </div>
-            </div>
 
-            <div class="relative flex flex-col rounded-xl mx-4 my-4 bg-gradient-to-r from-blue-200 to-blue-600 bg-clip-border  text-blue-900 shadow-md">
-              <div class="p-6">
-                <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal">
-                  Bookmarked
-                </h5>
-              </div>
-            </div>
+        {modalOpen && (
+      <div id="myModal" className="modal">
+        <div className="modal-content ">
+          <span className="close cursor-pointer bg-white p-3" onClick={closeModal}>&times;</span>
+          <SendMoneyCard/>
+        </div>
+      </div>
+       )}
 
-            <div class="relative flex flex-col rounded-xl bg-gradient-to-r from-green-200 to-green-600 bg-clip-border text-green-900 shadow-md">
-              <div class="p-6">
-                <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal">
-                  Your Notes &nbsp;
-                </h5>
-              </div>
-            </div>
-
-            <div class="relative flex flex-col rounded-xl mx-4 my-4 bg-gray-200  bg-clip-border  ">
-              <div class="p-6">
-                <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal">
-                  <Lottery/>
-                </h5>
-              </div>
-            </div>
+        <div className="flex justify-center py-3">
+      <div className="flex flex-col items-center sm:flex-row mb-9">
+        <div className="relative flex flex-col rounded-xl bg-gradient-to-r from-yellow-100 to-yellow-500 bg-clip-border cursor-pointer text-yellow-900 shadow-md mx-4"
+        onClick={openModal}>
+          <div className="p-6">
+            <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal">
+              Send Coins
+            </h5>
+            
           </div>
         </div>
+
+        <div className="relative flex flex-col rounded-xl bg-gradient-to-r mx-4 my-4 from-green-200 to-green-600 bg-clip-border text-green-900 shadow-md">
+          <div className="p-6">
+            <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal">
+              Your Notes
+            </h5>
+          </div>
+        </div>
+
+        <div className="relative flex flex-col rounded-xl bg-gray-200 bg-clip-border my-4">
+          <div className="p-6">
+            <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal">
+              <Lottery />
+            </h5>
+          </div>
+        </div>
+      </div>
+     
+    </div>
+   
         <div>
           <div>
             <div className="flex flex-col justify-center h-full ">
@@ -132,12 +150,15 @@ const Newdashboard = () => {
               </div>
             </div>
           </div>
+         
         </div>
         <br />
         <br /> <br />
         <br /> <br />
         <br />
+      
       </DashLay>
+     
     </div>
   );
 };
