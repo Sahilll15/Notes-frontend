e5fcac57c9f4f33d226ca68130d53d2dac58fac6
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-10-14 17:19:17 +0530

    impdated done

--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -1,80 +1,54 @@
 import React, { useEffect, useState } from "react";
 import Alternates from "../components/Layout/Setting";
 import { getTodosByUserId, createTodo, updateTodo, deleteTodo } from "../redux/todo/todoActions";
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchImpDates } from "../redux/impDates/impDateActions";
+import ImpDatesCard from "../components/ImpDatesCard";
+
 const Notification = () => {
-  const dispatch = useDispatch()
-  // const [tasks, setTasks] = useState([]);
+  const dispatch = useDispatch();
 
   const todos = useSelector((state) => state?.todo?.todos) || [];
+  const impDates = useSelector((state) => state?.impDate?.impDates);
   const [newTask, setNewTask] = useState("");
 
   const handleNewTaskChange = (event) => {
     setNewTask(event.target.value);
   };
 
-
   const handleAddTask = async (e) => {
     e.preventDefault();
     await dispatch(createTodo({
-      title: newTask
-    }))
+      title: newTask,
+    }));
 
     setNewTask("");
-    await dispatch(getTodosByUserId())
-  }
+    await dispatch(getTodosByUserId());
+  };
 
-  const handleupdate = async (id) => {
+  const handleUpdate = async (id) => {
     await dispatch(updateTodo(id));
-    await dispatch(getTodosByUserId())
-  }
-
+    await dispatch(getTodosByUserId());
+  };
 
   useEffect(() => {
-    dispatch(getTodosByUserId())
-  }, [dispatch])
-
-
+    dispatch(getTodosByUserId());
+    dispatch(fetchImpDates());
+  }, [dispatch]);
 
   return (
     <Alternates>
       <div className="flex flex-col md:flex-row">
-        <div className="w-full md:w-1/3 p-4 ">
-          <div className=" mt-5">
-            <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-sm md:max-w-2xl mt-1  p-6">
-              <div className="font-semibold text-xl mb-2">Exam/Assignment Title</div>
-              <p className="text-gray-700 text-base">
-                Description of the exam/assignment goes here.
-              </p>
-              <p className="text-gray-700 text-base mt-2">
-                Due Date: October 31, 2023
-              </p>
-            </div>
+        {impDates?.length === 0 && <h1 className="text-2xl text-red-500">No Important Dates Found</h1>}
+        <div className="flex flex-col-reverse ">
+          {impDates?.map((imp) => (
 
-            <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-md md:max-w-2xl mt-1   p-6">
-              <div className="font-semibold text-xl mb-2">Exam/Assignment Title</div>
-              <p className="text-gray-700 text-base">
-                Description of the exam/assignment goes here.
-              </p>
-              <p className="text-gray-700 text-base mt-2">
-                Due Date: October 31, 2023
-              </p>
-            </div>
+            <ImpDatesCard imp={imp} />
 
-            <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-md md:max-w-2xl mt-1 p-6">
-              <div className="font-semibold text-xl mb-2">Exam/Assignment Title</div>
-              <p className="text-gray-700 text-base">
-                Description of the exam/assignment goes here.
-              </p>
-              <p className="text-gray-700 text-base mt-2">
-                Due Date: October 31, 2023
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
 
-        {/* Right side - 2/3 */}
-        <div className="xl:w-2/4 border-2  mx-auto mt-9">
+        <div className="xl:w-2/4 border-2 mx-auto mt-9">
           <div className="container border-2 bg-white rounded-xl overflow-hidden shadow-lg mx-auto p-4 text-center">
             <h1 className="text-3xl font-bold mb-4">My To-Do List</h1>
             <form onSubmit={handleAddTask}>
@@ -84,7 +58,6 @@ const Notification = () => {
                   className="flex-1 border rounded py-2 px-3"
                   type="text"
                   placeholder="Add a new task..."
-
                   onChange={handleNewTaskChange}
                 />
                 <button
@@ -96,30 +69,22 @@ const Notification = () => {
               </div>
             </form>
             <ul id="taskList">
-              {
-                todos?.length === 0 && <h1 className="text-2xl">No Task Found</h1>
-              }
+              {todos?.length === 0 && <h1 className="text-2xl text-red-500">No Task Found</h1>}
 
               {todos?.length !== 0 && Array.isArray(todos) && todos?.map((task, index) => (
-                <li
-                  key={index}
-                  className="flex items-center justify-between border-b border-gray-300 py-2"
-                >
+                <li key={index} className="flex items-center justify-between border-b border-gray-300 py-2">
                   <div className="flex items-center space-x-2 w-4/5">
                     <input
                       type="checkbox"
                       className="form-checkbox"
                       id={`complete-${index}`}
                       checked={task.completed}
-                      onClick={() => { handleupdate(task._id) }}
-
+                      onClick={() => { handleUpdate(task._id); }}
                     />
                     <div className="text-left">
-
-
                       <label
                         htmlFor={`complete-${index}`}
-                        className={task.completed ? "line-through text-lg" : "text-lg"}
+                        className={task.completed ? "line-through text-lg text-green-500" : "text-lg"}
                       >
                         {task.title}
                       </label>
@@ -127,12 +92,11 @@ const Notification = () => {
                     </div>
                   </div>
                   <div>
-
                     <button
                       className="text-white border-2 bg-red-500 rounded-md border-red-500 p-1 ml-1 hover:bg-red-600"
                       onClick={async () => {
-                        await dispatch(deleteTodo(task?._id))
-                        await dispatch(getTodosByUserId())
+                        await dispatch(deleteTodo(task?._id));
+                        await dispatch(getTodosByUserId());
                       }}
                     >
                       Delete
