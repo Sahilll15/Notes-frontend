c2ac3ed098cdcbf15dfee94c45a219aacc335382
Aditya <192105adityashah@gmail.com>
2023-10-18 21:28:54 +0530

    fixed cancel spelling

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -235,7 +235,7 @@ const Setting = () => {
                               className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mr-2"
                               type="submit"
                             >
-                              Cancle
+                              Cancel
                             </button>
                           </>
                         ) : (
