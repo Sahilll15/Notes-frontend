cbea996929d6a57bcb5526df84e79b1333b1184e
Aditya <192105adityashah@gmail.com>
2023-10-18 21:34:08 +0530

    fixed sidebar link

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -268,7 +268,7 @@ export const SideBar = () => {
               </NavLink>
 
               {/* New Submission Button */}
-              <NavLink to={"/submission"}>
+              <NavLink to={"/notification"}>
                 <a
                   href="#"
                   className="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1"
